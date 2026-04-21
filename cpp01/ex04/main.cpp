b486a87108e8fb9a3416636ecfbdc408186194e1
pnurmi <pnurmi.zt@gmail.com>
2026-04-21 15:55:21 +0300

    end of day push

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+	if(argc != 4)
+		std::cout << "Please enter a ./FileName and two strings";
+	std::ifstream inputFile("example.txt");
+}
+

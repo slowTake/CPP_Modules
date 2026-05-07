4118066da8d42cb58e8b1cde13c012ce3817aba0
pnurmi <pnurmi.zt@gmail.com>
2026-05-07 16:12:49 +0300

    end of day push

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -12,4 +12,3 @@ void Zombie::announce(void)
 {
 	std::cout << this-> _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
-
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -8,11 +8,25 @@ class Fixed
 {
 	private:
 		int _value;
+		static const int _bits = 8;
+
 	public:
+		// Ortho Form
 		Fixed();
 		Fixed(const Fixed &copy);
 		Fixed &operator=(const Fixed &copy);
 		~Fixed();
+
+		// New Constructors
+		Fixed(const int n);
+		Fixed(const float f);
+
+		// Member Funcs
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
 };
+std::ostream &operator<<(std::ostream &o, Fixed const &i);
 
 #endif

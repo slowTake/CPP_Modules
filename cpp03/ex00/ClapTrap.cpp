0c81c38208b87a8e48d68661470fc568910da0f0
pnurmi <pnurmi.zt@gmail.com>
2026-05-09 14:45:23 +0300

    started ex00 and created cpp and hpp files WIP

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -1,2 +1,49 @@
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap(std::string name) : name(name), hp(10), energy(10), damage(0)
+{
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copy)
+{
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &copy)
+{
+}
+
+ClapTrap::~ClapTrap()
+{
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (this->hp <= 0 || this->energy <= 0)
+	{
+		std::cout << "ClapTrap " << this->name << " has no energy or hp left to attack";
+		return ;
+	}
+
+	this->energy -= 1;
+	std::cout << "ClapTrap " << this->name << " attacks" << target 
+	<< ", causing" << damage << " points of damage!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if(this->hp <= 0)
+	{
+		std::cout << "ClapTrap is already dead";	
+		return;
+	}
+
+	if(this->hp > 0)
+	{
+		std::cout << "ClapTrap " << this->name << "took:" << this->damage << " Damage";
+		this->hp -= this->hp - this->damage;
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+}
--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -1,18 +1,28 @@
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
 
-#include <string>
+# include <string>
+#include <iostream>
 
 class ClapTrap
 {
-	private:
-		std::string name;
-		unsigned int hp = 10;
-		unsigned int energy = 10;
-		unsigned int damage = 0;
+  private:
+	std::string name;
+	unsigned int hp = 10;
+	unsigned int energy = 10;
+	unsigned int damage = 0;
 
-	public:
-		
+  public:
+	// Ortho
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &copy);
+	ClapTrap &operator=(const ClapTrap &copy);
+	~ClapTrap();
+
+	// Member functions
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
 
 };
 

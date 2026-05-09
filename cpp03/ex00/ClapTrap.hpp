df3677cae2215a9f5b28e2f5c33545768f19eeca
pnurmi <pnurmi.zt@gmail.com>
2026-05-09 12:21:46 +0300

    started ex03

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+	private:
+		std::string name;
+		unsigned int hp = 10;
+		unsigned int energy = 10;
+		unsigned int damage = 0;
+
+	public:
+		
+
+};
+
+#endif

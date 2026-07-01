6aa262afaa707cf8554fdea848d3655b06b66ea6
pnurmi <pnurmi.zt@gmail.com>
2026-07-01 16:22:39 +0300

    tested and added pdf to root

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+    private:
+        const std::string   _name;
+        int                 _grade;
+
+    public:
+        // Orthodox Canonical Form
+        Bureaucrat();
+        Bureaucrat(const std::string &name, int grade);
+        Bureaucrat(const Bureaucrat &copy);
+        Bureaucrat &operator=(const Bureaucrat &copy);
+        ~Bureaucrat();
+
+        // Getters
+        std::string getName() const;
+        int         getGrade() const;
+
+        // Grade Shifting
+        void        incrementGrade();
+        void        decrementGrade();
+
+        // Custom Exception Classes
+        class GradeTooHighException : public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+};
+
+// Ostream overload
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
